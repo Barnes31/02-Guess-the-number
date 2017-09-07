3cc336a5430786c4b1e17577b0821b1bdde32b0f
Kayla Barnes <barnes31@umail.iu.edu>
2017-09-06 22:39:03 -0400

    adds several lines of code to create a working game

--- 02 guess.py
+++ 02 guess.py
@@ -4,28 +4,46 @@ import sys
 import random
 assert sys.version_info >= (3,4), "This script requires at least Python 3.4"
 
+maxGuess = 4
+player = input('Please type your name: ')
 
-#generate a random integer between 1 and 20 (inclusive) and store it in the variable [number]
-number = random.randint(1,20)
+    
+number = random.randint(1,10)
+print('Hello ' + player + '!' + ' Welcome to Guess That Number!')
 
-
-
-#ask the user for a response and store it in the variable [guess]
-guess = input()
-
-
-
-#a try/except block is a great tool for programmers to be able to deal with errors. In this instance, it reports an error if the user enters something other than an integer
-try:
-	#convert the guess to an integer
-	guess = int(guess)
+print('Im thinking of a number between 1 an 10. Can you guess what it is?')
+      
+print('But! You will have to guess carfully! You will only have' + " " + (str(maxGuess)) + " " + 'guesses!')
+      
+print('Lets begin!')
 
 
-	#check if the guess is less than the random number
-	if guess < number:
-		print('Too low!')
 
 
-except ValueError:
-	print('Please enter a whole number.')
+for GuessCount in range(maxGuess):
+        try:
+                guess = input('Whats your guess, ' + player + "? ")
+                guess = int(guess)
+                if guess < number and (maxGuess - (GuessCount + 1)) > 0:
+                        print('Oops too low!' + ' You have ' + (str(maxGuess - (GuessCount + 1))) + ' guesses left! Try again!')
+                elif guess > number and (maxGuess - (GuessCount + 1)) == 0:
+                        print('Oops too high!' + ' You have ' + (str(maxGuess - (GuessCount + 1))) + ' guesses left!')
+                elif guess > number and (maxGuess - (GuessCount + 1)) > 0:
+                        print('Oops too high!' + ' You have ' + (str(maxGuess - (GuessCount + 1))) + ' guesses left! Try again!')
+                elif guess < number and (maxGuess - (GuessCount + 1)) == 0:
+                        print('Oops too high!' + ' You have ' + (str(maxGuess - (GuessCount + 1))) + ' guesses left!')
+                elif guess == number:
+                        print('Way to go! Thats the one!')
+                        break
+
+
+        except ValueError:
+                print('Please enter a whole number.')
+
+if guess != number:
+      number = str(number)
+      print('Uh oh seems like you ran out of lives! The number I was thinking of was ' + number + '.' + ' Better luck next time!')
+
+if guess == number:
+        print('Congratulations, ' + player + '!' + ' You have won...bragging rights!')
 
